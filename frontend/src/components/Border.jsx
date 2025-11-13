b7831f6f99c1b0a5f0b203d58c412389b0536b5e
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-13 23:07:40 +0100

    types: add proptypes to Border

--- frontend/src/components/Border.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Border = ({ style, children }) => {
-    return (<div style={ Object.assign({ border: 'solid 1px #e8e8e8', borderRadius:'7px'}, style)}>
-        {children}
-    </div>);
-};
-
-export default Border;
