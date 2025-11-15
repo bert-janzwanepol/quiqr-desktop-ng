02e224276b53aa61018511d7fc58b3c3c40bbf6b
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-15 23:29:11 +0100

    chore: remove unused typeguard

--- frontend/src/utils/type-guards.ts
+++ frontend/src/utils/type-guards.ts
@@ -7,9 +7,3 @@ type PreviewConfig = { enable: boolean, preview_url: string }
 export const isValidPreviewConfiguration = (config: unknown): config is PreviewConfig => {
   return typeof config === "object" && "enable" in config && typeof config.enable === "boolean" && 'preview_url' in config && typeof config.preview_url === 'string';
 }
-
-// Maybe we dont need this. We can just use:
-// if (isValidPreviewConfiguration(config) && config.enable)
-export const hasPreviewCheckConfigurationEnabled = (config: unknown): config is { enable: true } => {
-  return typeof config === "object" && "enable" in config && (typeof config.enable === "boolean" || config.enable === true || config.enable === "true");
-}
