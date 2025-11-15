b4dcbd238a5116c2b1f178a5058d2962aa34f507
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-15 23:05:11 +0100

    fix: convert classType from string to React.ComponentType

--- frontend/src/components/HoForm/component-registry.ts
+++ frontend/src/components/HoForm/component-registry.ts
@@ -1,26 +1,30 @@
 import * as React from 'react';
-import type {ComponentProps, FieldBase} from './types';
-import BaseDynamic from './BaseDynamic';
+import type {FieldBase} from './types';
+import BaseDynamic, { BaseDynamicProps } from './BaseDynamic';
+
+// Type for class constructors that extend BaseDynamic
+// TODO: we probably don't need this once we refactor to functional components
+type BaseDynamicConstructor = new () => BaseDynamic;
 
 export class ComponentRegistry{
 
     dynamicComponents: {
         [key: string] : {
-            classType: string,
-            proplessInstance: BaseDynamic<FieldBase, any>
+            classType: React.ComponentType<BaseDynamicProps<FieldBase>>,
+            proplessInstance: BaseDynamic
         }
     };
 
-    constructor(componentTypes: Array<React.ComponentType<ComponentProps<FieldBase>>>){
+    constructor(componentTypes: Array<React.ComponentType<BaseDynamicProps<FieldBase>>>){
         this.dynamicComponents = {};
         for(let i = 0; i < componentTypes.length; i++){
             this.register(componentTypes[i]);
         }
     }
 
-    register(classType: React.ComponentType<ComponentProps<FieldBase>>){
+    register(classType: React.ComponentType<BaseDynamicProps<FieldBase>>){
         //$FlowFixMe
-        let proplessInstance = new classType();
+        let proplessInstance = new (classType as BaseDynamicConstructor)();
         //$FlowFixMe
         this.dynamicComponents[proplessInstance.getType()] = {
             classType,
