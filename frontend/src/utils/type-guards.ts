5a8cc66b4c477ea1e041417ca24e40ded7f555b2
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-15 23:28:55 +0100

    refactor: extract PreviewConfig to separate type

--- frontend/src/utils/type-guards.ts
+++ frontend/src/utils/type-guards.ts
@@ -2,7 +2,9 @@ export const hasOpenAiApiKey = (config: unknown): config is { openAiApiKey: stri
   return Boolean(typeof config === "object" && "openAiApiKey" in config && typeof config.openAiApiKey === "string");
 }
 
-export const isValidPreviewConfiguration = (config: unknown): config is { enable: boolean, preview_url: string } => {
+type PreviewConfig = { enable: boolean, preview_url: string }
+
+export const isValidPreviewConfiguration = (config: unknown): config is PreviewConfig => {
   return typeof config === "object" && "enable" in config && typeof config.enable === "boolean" && 'preview_url' in config && typeof config.preview_url === 'string';
 }
 
